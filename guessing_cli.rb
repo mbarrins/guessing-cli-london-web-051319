9c4f769d7ee450fc96c9819afc274cc4f753fee5
Maija Barrins <maija.barrins@gmail.com>
2019-04-30 15:07:59 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,12 +9,8 @@ def run_guessing_game
     guess = gets.chomp
 
     if guess.to_i == comp_guess
-      # puts comp_guess.class.to_s
-      # puts guess.class.to_s
-      # puts "Comp guess: #{comp_guess} & guess: #{guess}"
       puts "You guessed the correct number!"
     else
-      # puts "Comp guess: #{comp_guess} & guess: #{guess}"
       puts "The computer guessed #{comp_guess}."
     end
   end
