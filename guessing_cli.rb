63164b75ae1b57c8272d7c68a359892f21545ab6
Maija Barrins <maija.barrins@gmail.com>
2019-04-30 14:57:14 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   comp_guess = rand(1..6)
   guess = gets.chomp
-
+  binding.pry
   if guess == "exit"
     puts "Goodbye!"
   elsif guess == comp_guess.to_i
